f27925622ad07b743906ead4472185c7318cb6a1
Fernanda Pinto Lopes <fpl.snf19@uea.edu.br>
2021-05-14 16:40:11 -0400

    Ajuste do tempo de execução

--- ordenacoes.cpp
+++ ordenacoes.cpp
@@ -113,26 +113,26 @@ int main (){
     auto inicio = std::chrono::high_resolution_clock::now();
     ordenaCrescente(array1, 100000);
     auto resultado = std::chrono::high_resolution_clock::now() - inicio;
-    long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(resultado).count();
-    cout << "array 1 Ordena Crescente: " << microseconds << " microssegundos\n";
+    long long milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(resultado).count();
+    cout << "array 1 Ordena Crescente: " << milliseconds << " milisegundos\n";
     
     inicio = std::chrono::high_resolution_clock::now();
     ordenaDecrescente(array2, 100000);
     resultado = std::chrono::high_resolution_clock::now() - inicio;
-    microseconds = std::chrono::duration_cast<std::chrono::microseconds>(resultado).count();
-    cout << "array 2 Ordena Decrescente: " << microseconds << " microssegundos\n";
+    milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(resultado).count();
+    cout << "array 2 Ordena Decrescente: " << milliseconds << " milisegundos\n";
 
     inicio = std::chrono::high_resolution_clock::now();
     ordenaCrescenteDecrescente(array3, 100000); 
     resultado = std::chrono::high_resolution_clock::now() - inicio;
-    microseconds = std::chrono::duration_cast<std::chrono::microseconds>(resultado).count();
-    cout << "array 3 Ordena Crescente e Decrescente: " << microseconds << " microssegundos\n";
+    milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(resultado).count();
+    cout << "array 3 Ordena Crescente e Decrescente: " << milliseconds << " milisegundos\n";
 
     inicio = std::chrono::high_resolution_clock::now();
     ordenaDecrescenteCrescente(array4, 100000);    
     resultado = std::chrono::high_resolution_clock::now() - inicio;
-    microseconds = std::chrono::duration_cast<std::chrono::microseconds>(resultado).count();
-    cout << "array 4 Ordena Decrescente e Crescente: " << microseconds << " microssegundos\n";
+    milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(resultado).count();
+    cout << "array 4 Ordena Decrescente e Crescente: " << milliseconds << " milisegundos\n";
 
     
     return 0;
