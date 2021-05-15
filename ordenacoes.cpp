2e36fce91b128de36e0ff6881532618f84891e0f
vanessadcamara <vanessadcamara@gmail.com>
2021-05-15 10:26:36 -0400

    atualizando

--- ordenacoes.cpp
+++ ordenacoes.cpp
@@ -4,6 +4,7 @@ int array1[100000];
 int array2[100000];
 int array3[100000];
 int array4[100000];
+int array5[100000];
 
 void ordenaCrescente(int a[],int n){
 	int salto, j, menor_valor;
@@ -21,118 +22,59 @@ void ordenaCrescente(int a[],int n){
     }
 }
 
-void ordenaDecrescente(int a[], int n){
-	int salto, j, maior_valor;
-	for(salto = n/2; salto >= 1; salto /= 2){
-		for(int i = salto; i < n; i++){
-			maior_valor = a[i];
-			for(j = i; j >= salto; j-= salto){  
-                if(maior_valor > a[j-salto] == false) break;
-                else{
-                    a[j] = a[j-salto];
-                }
-            }
-			a[j] = maior_valor;
-		}
-    }
-}
-void ordenaCrescenteDecrescente(int a[], int n){
-    int salto, j, menor_valor;
-	for(salto = n/2; salto >= 1; salto /= 2){
-		for(int i = salto; i < n; i++){
-			menor_valor = a[i];
-			for(j = i; j >= salto; j-= salto){  
-                if(menor_valor < a[j-salto] == false) break;
-                else{
-                    a[j] = a[j-salto];
-                }
-            }
-			a[j] = menor_valor;
-		}
-    }
-    int maior_valor;
-	for(salto = n/2; salto >= 1; salto /= 2){
-		for(int i = salto; i < n; i++){
-			if(i < n/2) continue;
-            maior_valor = a[i];
-
-			for(j = i; j >= salto; j-= salto){  
-                if(j < n/2) break;
-                if(maior_valor > a[j-salto] == false) break;
-                else{
-                    a[j] = a[j-salto];
-                }
-            }
-			a[j] = maior_valor;
-		}
-    }
-    
-}
-void ordenaDecrescenteCrescente(int a[], int n){
-    // ordena todo o array de forma crescente
-    int salto, j, menor_valor, maior_valor;
-	for(salto = n/2; salto >= 1; salto /= 2){
-		for(int i = salto; i < n; i++){
-			menor_valor = a[i];
-			for(j = i; j >= salto; j-= salto){  
-                if(menor_valor < a[j-salto] == false) break;
-                else{
-                    a[j] = a[j-salto];
-                }
-            }
-			a[j] = menor_valor;
-		}
-    }
-	for(salto = n/2; salto >= 1; salto /= 2){
-		for(int i = salto; i < n; i++){
-            if(i >= n/2) continue;
-			maior_valor = a[i];
-			for(j = i; j >= salto; j-= salto){ 
-                if(j >= n/2) break; 
-                if(maior_valor > a[j-salto] == false) break;
-                else{
-                    a[j] = a[j-salto];
-                }
-            }
-			a[j] = maior_valor;
-		}
-    }
-	
-}
-
-int main (){
-    int max = 100000;
-    int min = 1;
-    for(int i = 0; i < 100000; i++){
-        int aleatorio = rand()%(max-min+1) + min;
-        array1[i] = aleatorio;
-        array2[i] = aleatorio;
-        array3[i] = aleatorio;
-        array4[i] = aleatorio;
-    }
+long long tempoDeOrdenacao(int array[], int tamanho){
     auto inicio = std::chrono::high_resolution_clock::now();
-    ordenaCrescente(array1, 100000);
+    ordenaCrescente(array, tamanho);
     auto resultado = std::chrono::high_resolution_clock::now() - inicio;
-    long long milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(resultado).count();
-    cout << "array 1 Ordena Crescente: " << milliseconds << " milisegundos\n";
+    long long milissegundos = std::chrono::duration_cast<std::chrono::milliseconds>(resultado).count();
+    return milissegundos;
+
+}
+int main (){
+    int tamanho_max = 100000, min = 1;
+
+    // aleatório -> crescente
+    for(int i = 0; i < tamanho_max; i++){
+        int aleatorio = rand()%(tamanho_max-min+1) + min;
+        array1[i] = aleatorio;
+    }
+    cout << "Aleatório -> Crescente: " << tempoDeOrdenacao(array1, tamanho_max) << "ms\n";
     
-    inicio = std::chrono::high_resolution_clock::now();
-    ordenaDecrescente(array2, 100000);
-    resultado = std::chrono::high_resolution_clock::now() - inicio;
-    milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(resultado).count();
-    cout << "array 2 Ordena Decrescente: " << milliseconds << " milisegundos\n";
+    // crescente -> crescente
+    for(int i = 0; i < tamanho_max; i++){
+        array2[i] = array1[i];
+    }
+    cout << "Crescente -> Crescente: " << tempoDeOrdenacao(array2, tamanho_max) << "ms\n";
 
-    inicio = std::chrono::high_resolution_clock::now();
-    ordenaCrescenteDecrescente(array3, 100000); 
-    resultado = std::chrono::high_resolution_clock::now() - inicio;
-    milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(resultado).count();
-    cout << "array 3 Ordena Crescente e Decrescente: " << milliseconds << " milisegundos\n";
+    // decrescente -> crescente
+    for(int i = 0, j = tamanho_max; i < tamanho_max; i++, j--){
+        array3[i] = j;
+    }
+    cout << "Decrescente -> Crescente: " << tempoDeOrdenacao(array3, tamanho_max) << "ms\n";
 
-    inicio = std::chrono::high_resolution_clock::now();
-    ordenaDecrescenteCrescente(array4, 100000);    
-    resultado = std::chrono::high_resolution_clock::now() - inicio;
-    milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(resultado).count();
-    cout << "array 4 Ordena Decrescente e Crescente: " << milliseconds << " milisegundos\n";
+    // decrescente/crescente -> crescente
+    for(int i = tamanho_max/2, j = 0; i >= 0; i--, j++){
+        array4[j] = array3[i];
+    }
+    for(int i = tamanho_max/2+1; i <= tamanho_max; i++){
+        array4[i] = array3[i];
+    } 
+    cout << "Decrescente/Crescente -> Crescente: " << tempoDeOrdenacao(array4, tamanho_max) << "ms\n";
+
+    // crescente/decrescente -> crescente
+    for(int i = 0; i <= tamanho_max/2; i++){
+        array5[i] = array4[i];
+    }
+    for(int i = tamanho_max/2, j = tamanho_max; i < tamanho_max; i++, j--){
+        array5[j] = array4[i];
+    }
+    cout << "Crescente/Decrescente -> Crescente: " << tempoDeOrdenacao(array5, tamanho_max) << "ms\n";
+
+    // auto inicio = std::chrono::high_resolution_clock::now();
+    // ordenaCrescente(array1, 100000);
+    // auto resultado = std::chrono::high_resolution_clock::now() - inicio;
+    // long long milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(resultado).count();
+    // cout << "array 1 Ordena Crescente: " << milliseconds << " milisegundos\n";
 
     
     return 0;
