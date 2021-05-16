37ca49368d93a039b5a2a325d1662ff503b51887
vanessadcamara <vanessadcamara@gmail.com>
2021-05-16 14:04:31 -0400

    atualizando

--- ordenacoes.cpp
+++ ordenacoes.cpp
@@ -33,48 +33,42 @@ long long tempoDeOrdenacao(int array[], int tamanho){
 int main (){
     int tamanho_max = 100000, min = 1;
 
-    // aleatório -> crescente
+    // aleatório -> ordenado
     for(int i = 0; i < tamanho_max; i++){
         int aleatorio = rand()%(tamanho_max-min+1) + min;
         array1[i] = aleatorio;
     }
-    cout << "Aleatório -> Crescente: " << tempoDeOrdenacao(array1, tamanho_max) << "ms\n";
+    cout << "Aleatório -> Ordenado: " << tempoDeOrdenacao(array1, tamanho_max) << "ms\n";
     
-    // crescente -> crescente
+    // ordenado -> ordenado
     for(int i = 0; i < tamanho_max; i++){
         array2[i] = array1[i];
     }
-    cout << "Crescente -> Crescente: " << tempoDeOrdenacao(array2, tamanho_max) << "ms\n";
+    cout << "Ordenado -> Ordenado: " << tempoDeOrdenacao(array2, tamanho_max) << "ms\n";
 
-    // decrescente -> crescente
+    // decrescente -> ordenado
     for(int i = 0, j = tamanho_max; i < tamanho_max; i++, j--){
         array3[i] = j;
     }
-    cout << "Decrescente -> Crescente: " << tempoDeOrdenacao(array3, tamanho_max) << "ms\n";
+    cout << "Decrescente -> Ordenado: " << tempoDeOrdenacao(array3, tamanho_max) << "ms\n";
 
-    // decrescente/crescente -> crescente
+    // decrescente/ordenado -> ordenado
     for(int i = tamanho_max/2, j = 0; i >= 0; i--, j++){
         array4[j] = array3[i];
     }
     for(int i = tamanho_max/2+1; i <= tamanho_max; i++){
         array4[i] = array3[i];
     } 
-    cout << "Decrescente/Crescente -> Crescente: " << tempoDeOrdenacao(array4, tamanho_max) << "ms\n";
+    cout << "Decrescente/Ordenado -> Ordenado: " << tempoDeOrdenacao(array4, tamanho_max) << "ms\n";
 
-    // crescente/decrescente -> crescente
+    // ordenado/decrescente -> ordenado
     for(int i = 0; i <= tamanho_max/2; i++){
         array5[i] = array4[i];
     }
     for(int i = tamanho_max/2, j = tamanho_max; i < tamanho_max; i++, j--){
         array5[j] = array4[i];
     }
-    cout << "Crescente/Decrescente -> Crescente: " << tempoDeOrdenacao(array5, tamanho_max) << "ms\n";
-
-    // auto inicio = std::chrono::high_resolution_clock::now();
-    // ordenaCrescente(array1, 100000);
-    // auto resultado = std::chrono::high_resolution_clock::now() - inicio;
-    // long long milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(resultado).count();
-    // cout << "array 1 Ordena Crescente: " << milliseconds << " milisegundos\n";
+    cout << "Ordenado/Decrescente -> Ordenado: " << tempoDeOrdenacao(array5, tamanho_max) << "ms\n";
 
     
     return 0;
